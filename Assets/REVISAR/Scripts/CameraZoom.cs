786fc30279f90ad13c16def852941af7c0667afb
nemorisgames <contacto@nemorisgames.com>
2016-09-15 09:54:52 -0300

    First upload
    AC y NGUI added

---
+++ Assets/REVISAR/Scripts/CameraZoom.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+
+    
+     
+        public float rango = 10;
+        public Transform target;
+        public float smoothing = 5f;
+        Vector3 offset;
+
+        void Start()
+        {
+            offset = transform.position - target.position;
+        }
+
+
+        void FixedUpdate()
+        {
+                
+
+            Vector3 targetCampos = target.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetCampos, smoothing * Time.deltaTime);
+
+        }
+    }
