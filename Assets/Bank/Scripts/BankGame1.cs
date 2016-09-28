9a67c9ad9d5d224bb96c11d312bd75988194e1ec
njaramillo-ng <njaramillo@nemorisgames.com>
2016-09-28 18:36:24 -0300

    Bank updated

---
+++ Assets/Bank/Scripts/BankGame1.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using AC;
+using System.Collections.Generic;
+
+public class BankGame1 : BankGame_Base 
+{
+	public Transform finishPoint; //punto de guardado de los billetes correctos
+
+	void Start ()
+	{
+		count = totalBills;
+		initialPositions = new Vector3[totalBills];
+		addDistance = new Vector3 (-distance, 0, 0);
+		randomGenerate = true;
+		//GenerateBills ();
+	}
+
+	void Update()
+	{
+		if (!AC.LocalVariables.GetBooleanValue (4)) 
+		{
+			//Temporal: con tecla R, todos los billetes vuelven a su lugar. Remover después
+			if (Input.GetKeyDown (KeyCode.R)) 
+			{
+				RestoreBills ();
+				count = totalBills;
+			};
+			if (Input.GetKeyDown (KeyCode.K) || count <= 0)
+			{
+				EndGame (4,3);
+				float score = AC.LocalVariables.GetFloatValue (3);
+				if (score > 20) {
+					AC.LocalVariables.SetIntegerValue (8, 1);
+				} 
+				else if (score < 20 && score > 10) {
+					AC.LocalVariables.SetIntegerValue (8, 2);
+				} 
+				else if (score >= 0 && score < 10) {
+					AC.LocalVariables.SetIntegerValue (8, 3);
+				}
+				count = totalBills;
+			};
+		}
+	}
+
+	public void SaveBill(int id)
+	{
+		billsList [id].GetComponent<Rigidbody> ().MovePosition (finishPoint.position);
+		count -= 1;
+	}
+}
