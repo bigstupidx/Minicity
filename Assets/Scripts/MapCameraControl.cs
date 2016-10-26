9f46f5e5e4821057c8fc7fbf00b4611ec24ddc70
nemorisgames <contacto@nemorisgames.com>
2016-10-26 15:18:47 -0300

    audios y personajes con animaciones añadidos

---
+++ Assets/Scripts/MapCameraControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapCameraControl : MonoBehaviour {
+	public AC.GameCamera camera;
+	private Vector2 orbitSpeed;
+	private float rotateSpeed;
+	private float zoomSpeed;
+
+	public float orbitSpeedModifier=1;
+	public float zoomSpeedModifier=5;
+	public float rotateSpeedModifier=1;
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnEnable(){
+		Gesture.onDraggingE += OnDragging;
+		//Gesture.onRotateE += OnRotate;
+		Gesture.onPinchE += OnPinch;
+	}
+
+	void OnDisable(){
+		Gesture.onDraggingE -= OnDragging;
+		//Gesture.onRotateE += OnRotate;
+		Gesture.onPinchE += OnPinch;
+	}
+
+	void OnDragging(DragInfo dragInfo){
+		orbitSpeed=dragInfo.delta*orbitSpeedModifier;
+	}
+
+
+	void OnPinch(float val){
+		zoomSpeed-=val*zoomSpeedModifier;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position=center;
+		transform.rotation*=Quaternion.Euler(-orbitSpeed.y, orbitSpeed.x, rotateSpeed);
+		//transform.position=transform.TransformPoint(new Vector3(0, 0, dist));
+
+		orbitSpeed*=(1-Time.deltaTime*3);
+		rotateSpeed*=(1-Time.deltaTime*4f);
+		zoomSpeed*=(1-Time.deltaTime*4);
+	}
+}
