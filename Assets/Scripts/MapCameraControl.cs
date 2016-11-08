8c6509d2a471f36df4bcb1141b2b3a8677b7fd88
nemorisgames <contacto@nemorisgames.com>
2016-11-08 09:26:35 -0300

    cambios al charactercontroller

--- Assets/Scripts/MapCameraControl.cs
+++ Assets/Scripts/MapCameraControl.cs
@@ -2,16 +2,19 @@
 using System.Collections;
 
 public class MapCameraControl : MonoBehaviour {
-	public AC.GameCamera camera;
-	private Vector2 orbitSpeed;
+	public Camera camera;
+	public Vector2 orbitSpeed;
 	private float rotateSpeed;
 	private float zoomSpeed;
 
 	public float orbitSpeedModifier=1;
 	public float zoomSpeedModifier=5;
 	public float rotateSpeedModifier=1;
+
+	Transform player;
 	// Use this for initialization
 	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
 
 	void OnEnable(){
@@ -27,18 +30,26 @@ public class MapCameraControl : MonoBehaviour {
 	}
 
 	void OnDragging(DragInfo dragInfo){
-		orbitSpeed=dragInfo.delta*orbitSpeedModifier;
+		orbitSpeed = dragInfo.delta * orbitSpeedModifier;
 	}
 
 
 	void OnPinch(float val){
-		zoomSpeed-=val*zoomSpeedModifier;
+		zoomSpeed -= val * zoomSpeedModifier;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//transform.position=center;
-		transform.rotation*=Quaternion.Euler(-orbitSpeed.y, orbitSpeed.x, rotateSpeed);
+		print (Input.mouseScrollDelta);
+		OnPinch(Input.mouseScrollDelta.y);
+
+		transform.position = player.position;
+		transform.Rotate (-orbitSpeed.y, orbitSpeed.x, 0f);
+		transform.rotation = Quaternion.Euler(new Vector3 (Mathf.Clamp(transform.rotation.eulerAngles.x, 330f, 350f), transform.rotation.eulerAngles.y, 0f));
+		camera.fieldOfView = Mathf.Clamp (camera.fieldOfView + zoomSpeed, 30f, 80f);
+
+		//transform.rotation = Quaternion.Euler(new Vector3 (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f));
+		//transform.rotation *= Quaternion.Euler (-orbitSpeed.y, orbitSpeed.x, rotateSpeed);
 		//transform.position=transform.TransformPoint(new Vector3(0, 0, dist));
 
 		orbitSpeed*=(1-Time.deltaTime*3);
--- Assets/Scripts/TaskList.cs
+++ Assets/Scripts/TaskList.cs
@@ -73,7 +73,7 @@ public class TaskList : MonoBehaviour {
 		if(scores != null)
 			finalScore = finalScore / (scores.Length + 1);
 
-		stars = Mathf.RoundToInt (finalScore);
+		stars = Mathf.FloorToInt (finalScore);
 		print ("score: " + stars);
 		MainController m = GameObject.FindGameObjectWithTag ("MainController").GetComponent<MainController>();
 		m.SaveScore (stars);
